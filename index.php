e52c2f89a32f48d35de93623b504d422b265ce3f
202203227 <234624142+unload0@users.noreply.github.com>
2026-05-25 02:22:35 +0300

    project upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "<h1>hello</h1>";
+        ?>
+    </body>
+</html>
